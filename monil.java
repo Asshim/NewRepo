97f46b304ad300ae8fe3b189b81aad1af4e0b96b
Asshim <poudeloshim16@gmail.com>
2024-04-25 12:07:24 +0545

    new code

---
+++ monil.java
@@ -0,0 +1,13 @@
+/*
+*hello
+*/ 
+public class hello{
+    public static void main(string[]args){
+        string x= "hello world";
+                string y= "hello ";
+                
+
+
+        system.out.println(x + y);5
+    }
+}
